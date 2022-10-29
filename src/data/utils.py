a22a634a2cb7a328fc7e32c6043cc3f62d28868b
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-10-28 21:23:31 -0300

    fix mnist load, adding cifar10, optional dataloader shuffle

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,21 +1,32 @@
-from avalanche.benchmarks import SplitMNIST
+from avalanche.benchmarks import SplitMNIST, SplitCIFAR10
 from torch.utils.data import DataLoader
 from torchsampler import ImbalancedDatasetSampler
+from torchvision import transforms
 
 
 def load_data(dataset, n_experiences=5):
-    if dataset == 'SplitMNIST':
+    transfs = transforms.ToTensor()
+    if dataset == 'MNIST':
         data = SplitMNIST(n_experiences=n_experiences,
-                          shuffle=True,
+                          shuffle=False,
                           dataset_root='data/datasets',
-                          return_task_id=True)
+                          return_task_id=False,
+                          train_transform=transfs,
+                          eval_transform=transfs)
+    elif dataset == 'CIFAR10':
+        data = SplitCIFAR10(n_experiences=n_experiences,
+                            shuffle=False,
+                            dataset_root='data/datasets',
+                            return_task_id=False,
+                            train_transform=transfs,
+                            eval_transform=transfs)
     else:
         raise ValueError(f'dataset {dataset} not supported.')
 
     return data
 
 
-def get_dataloader(dataset, batch_size, imbalanced_sampler=False):
+def get_dataloader(dataset, batch_size, imbalanced_sampler=False, shuffle=True):
     """
     Creates a dataloader for the given dataset.
 
@@ -34,7 +45,7 @@ def get_dataloader(dataset, batch_size, imbalanced_sampler=False):
     """
 
     sampler = ImbalancedDatasetSampler(dataset) if imbalanced_sampler else None
-    shuffle = False if sampler is not None else True
+    shuffle = False if sampler is not None else shuffle
     dataloader = DataLoader(dataset=dataset,
                             num_workers=4,
                             shuffle=shuffle,

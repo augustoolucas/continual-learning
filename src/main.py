25ca262bd459d0339f503601144cc0b76053e803
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-10-28 21:25:17 -0300

    continual learning + end-to-end training

--- src/main.py
+++ src/main.py
@@ -1,13 +1,48 @@
+import pytorch_lightning as pl
+
 import models
+from cfg import test_cfg, train_cfg
 from data import utils
 
-benchmark = utils.load_data('SplitMNIST')
+benchmark = utils.load_data("MNIST")
+
+encoder = models.MLPModels.Encoder(img_shape=(1, 28, 28), latent_dim=32)
+decoder = models.MLPModels.Decoder(img_shape=(1, 28, 28),
+                                   latent_dim=32,
+                                   n_classes=10)
+
+specific = models.MLPModels.Specific(img_shape=(1, 28, 28), specific_size=20)
+classifier = models.MLPModels.Classifier(latent_size=32,
+                                         specific_size=20,
+                                         n_classes=10)
+
+ircl = models.IRCL.IRCL(specific, classifier, encoder, decoder)
 
 train_stream = benchmark.train_stream
+test_stream = benchmark.test_stream
 
-model = models.Encoder((1, 28, 28), 32)
+# Continual Learning
 
-for experience in train_stream:
-    for img, label, task_id in experience.dataset:
-        model(img.unsqueeze(0))
-        breakpoint()
+for train_exp, test_exp in zip(train_stream, test_stream):
+    train_loader = utils.get_dataloader(train_exp.dataset,
+                                        train_cfg["batch_size"])
+    test_loader = utils.get_dataloader(test_exp.dataset,
+                                       test_cfg["batch_size"],
+                                       shuffle=False)
+    trainer = pl.Trainer(max_epochs=train_cfg["epochs"], accelerator="gpu")
+    trainer.fit(ircl, train_loader)
+    trainer.test(dataloaders=test_loader, ckpt_path="best")
+
+test_loader = utils.get_dataloader(benchmark.original_test_dataset,
+                                   test_cfg["batch_size"],
+                                   shuffle=False)
+
+trainer.test(dataloaders=test_loader, ckpt_path="best")
+
+# End to end
+train_loader = utils.get_dataloader(benchmark.original_train_dataset,
+                                    train_cfg["batch_size"])
+
+trainer = pl.Trainer(max_epochs=train_cfg["epochs"], accelerator="gpu")
+trainer.fit(ircl, train_loader)
+trainer.test(dataloaders=test_loader, ckpt_path="best")
